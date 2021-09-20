bdf41dff8f5fbf2455048ae1a7cc4f82c8a6706b
elinwonyoungpark <elinpark.102@gmail.com>
2021-09-20 16:38:09 +0900

    sum_sub_result

---
+++ Calculator_v02/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator_v02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void sumbtn_Click(object sender, EventArgs e)
+        {
+            int number1 = Convert.ToInt32(num1.Text);
+            int number2 = Convert.ToInt32(num2.Text);
+            int sum = number1 + number2;
+            result.Text = sum.ToString();
+        }
+
+        private void subbtn_Click(object sender, EventArgs e)
+        {
+            int number1 = Convert.ToInt32(num1.Text);
+            int number2 = Convert.ToInt32(num2.Text);
+            int sub = number1 - number2;
+            result.Text = sub.ToString();
+        }
+
+    }
+}
